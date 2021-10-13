4a545d3d27a8ec746dcf55f67b9b4a043929d172
Mobin A Piracha <mobinapiracha@gmail.com>
2021-10-13 10:45:17 -0500

    update

--- plots.js
+++ plots.js
@@ -29,7 +29,7 @@ function buildMetadata(sample) {
     PANEL.html("");
     Object.entries(result).forEach(([key, value]) => {
       PANEL.append("h6").text(`${key}: ${value}`);
-    })
+    });
 
   });    
   };
